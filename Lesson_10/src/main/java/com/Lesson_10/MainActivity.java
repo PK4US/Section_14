1948dbcc55035d9832ba9de84c9b7b49cadb87ba
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-11-10 14:04:57 +0200

    Урок 10. Анимация своих изображений

--- Lesson_10/src/main/java/com/Lesson_10/MainActivity.java
+++ Lesson_10/src/main/java/com/Lesson_10/MainActivity.java
@@ -1,8 +1,10 @@
 package com.Lesson_10;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView iv = findViewById(R.id.imageView);
+        Animation imageAnim = AnimationUtils.loadAnimation(this, R.anim.image_animation);
+        iv.startAnimation(imageAnim);
+
+        ImageView clock = findViewById(R.id.clock);
+        Animation clockAnim = AnimationUtils.loadAnimation(this, R.anim.clock_animation);
+        clock.startAnimation(clockAnim);
     }
 }
