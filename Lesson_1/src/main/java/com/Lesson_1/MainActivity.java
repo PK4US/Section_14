8012b5a30323943c0d3b970bf5430c4f0e485e39
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-11-10 13:59:07 +0200

    Урок 1. Основы рисования в Android

--- Lesson_1/src/main/java/com/Lesson_1/MainActivity.java
+++ Lesson_1/src/main/java/com/Lesson_1/MainActivity.java
@@ -1,14 +1,26 @@
 package com.Lesson_1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new MyDraw(this));
+    }
+
+    private class MyDraw extends View {
+        public MyDraw(Context context) {
+            super(context);
+        }
+        protected void onDraw(Canvas canvas) {
+            canvas.drawColor(Color.YELLOW);
+        }
     }
 }
