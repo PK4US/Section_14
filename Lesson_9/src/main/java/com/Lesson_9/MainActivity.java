0d86ad9871a4d0fb540f216ec89ce9f870ef90f3
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-11-10 14:04:37 +0200

    Урок 9. Анимация компонентов

--- Lesson_9/src/main/java/com/Lesson_9/MainActivity.java
+++ Lesson_9/src/main/java/com/Lesson_9/MainActivity.java
@@ -1,14 +1,29 @@
-Lesson_9package com.Lesson_9;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.Lesson_9;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean start = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Animation animation = AnimationUtils.loadAnimation(this, R.anim.my_animation);
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start = !start;
+                if (start) view.startAnimation(animation);
+                else view.clearAnimation();
+            }
+        });
     }
 }
